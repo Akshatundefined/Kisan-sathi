38fee7e83c5c4f60cf878e89fbcb3038a351a167
Akshatundefined <akshat15052008@gmail.com>
2026-09-13 23:09:27 +0530

    Deploy backend with Netlify functions

--- server.js
+++ server.js
@@ -211,4 +211,10 @@ app.get('/api/farmers/:id/notifications', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => console.log('Server running on port ' + process.env.PORT));
+if (require.main === module) {
+  app.listen(process.env.PORT || 3000, () =>
+    console.log('Server running on port ' + (process.env.PORT || 3000))
+  );
+}
+
+module.exports = app;
